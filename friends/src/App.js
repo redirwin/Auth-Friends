fd792fe57629add62fe16404ec4b78762b463402
Dave Irwin <david@redirwin.com>
2019-07-18 12:03:47 -0700

    created axiosWithAuth, added imports App.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     My App
-    </div>
-  );
+  return <div className="App">My App</div>;
 }
 
 export default App;
